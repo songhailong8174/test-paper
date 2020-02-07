263487b63f54f412539cc72a74af4f6b55418066
shl <long@shldeMacBook-Pro.local>
2020-02-07 15:54:17 +0800

    Initial commit

---
+++ src/main/java/com/shl/testpaper/TestPaperApplication.java
@@ -0,0 +1,13 @@
+package com.shl.testpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPaperApplication.class, args);
+    }
+
+}
